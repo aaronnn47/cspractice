78225a82c074b8a51ca7ac5b8f5df68c9374de76
Aaron Kim <iamaaronkim@gmail.com>
2019-02-02 11:34:03 -0700

    practice

--- day4/projecteuler.cs
+++ day4/projecteuler.cs
@@ -5,9 +5,18 @@ class project
 
   static void Main()
   {
+    
+    int count = 0;
+
     for(int i = 0; i < 10; i++)
     {
-      Console.WriteLine(i);
+      if(i % 3 == 0)
+      {
+	Console.WriteLine("im divisible");
+      } 
+      else{
+        Console.WriteLine("im not");
+      }
     }
   }
 }
