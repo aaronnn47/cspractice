8bb34a3e24c0593c1074ee2bee002950d14e83e3
Aaron Kim <iamaaronkim@gmail.com>
2019-02-02 12:14:01 -0700

    fib attempt

--- day4/evenfibonacci/evenfibonacci.cs
+++ day4/evenfibonacci/evenfibonacci.cs
@@ -4,17 +4,8 @@ class fibonnaci
 {
   static void Main()
   {
-   int start = 1;
-   int next = 1;
-   int count = 0;
-
-   for(int i = start; i < 10; i++)
-   {
-     count = start + next;
-         
-   }
-
-   Console.WriteLine(count);
+  Console.WriteLine("hello");
   }
-
+    
+  
 }
