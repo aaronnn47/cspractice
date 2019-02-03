c46e1c5ccb3e843a7591b64eb67f0501fb9eebeb
Aaron Kim <iamaaronkim@gmail.com>
2019-02-03 14:29:55 -0700

    practice

--- day5/arraysum.cs
+++ day5/arraysum.cs
@@ -3,7 +3,7 @@ using System;
 class Solution
 {
   public int simpleArraySum(int[] ar){
-  int count = 0;
+  var count = 0;
   foreach(int i in ar){
     count = count + i;
   }
@@ -15,9 +15,8 @@ class Solution
   {
    int[] arr = {1,2,3,4,5};
    Solution sol = new Solution();
-   var i = 2;  
- 
-   Console.WriteLine(i);
+   
+   Console.WriteLine(sol.simpleArraySum(arr));
 
   }
 }
