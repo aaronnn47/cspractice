b0f6ba3a63ca79bfa5465cd581865f385d9390be
Aaron Kim <iamaaronkim@gmail.com>
2019-02-08 12:26:09 -0700

    solution

--- day10/lastword.cs
+++ day10/lastword.cs
@@ -9,11 +9,12 @@ namespace Practice
 		static void Main()
 		{
 			string solution = "hello world";
-
-			for(int i = solution.Length - 1; Char.IsWhiteSpace(solution[i]); i--)
+			
+			for(int i = solution.Length - 1; !Char.IsWhiteSpace(solution[i]); i--)
 			{
 				Console.Write(solution[i]);
 			}
+			Console.Write('\n');
 			
 		}
 	}
